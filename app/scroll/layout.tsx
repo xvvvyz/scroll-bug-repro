ea2c580a5da5a67c2d861b44ccc93e8d2b198c12
VillageR88 <villager88pl@gmail.com>
2024-03-22 16:21:00 +0100

    autoscroll fix
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/scroll/layout.tsx
+++ app/scroll/layout.tsx
@@ -1,11 +1,11 @@
 'use client';
 import { useEffect } from 'react';
 
-
 export default function Layout({ children }) {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  
   return (
     <div>
       <div
